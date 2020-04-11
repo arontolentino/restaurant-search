82a4dd6aac4a635241e9102fee989c77ace715f5
Aron Tolentino <arontolentino@outlook.com>
2020-04-11 17:30:39 -0400

    Create ResultsDetail component

---
+++ src/screens/ResultsShowScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ResultsShowScreen = () => {
+	return <View>Results Show</View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default ResultsShowScreen;
